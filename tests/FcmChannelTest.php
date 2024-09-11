5351fd1679cd2ed79db40cdc892d9b2b40a52412
Alexander Ginko <ahinko@neklo.com>
2024-09-11 17:06:49 +0200

    SWR-14682 Removed unsupported batch api.

--- tests/FcmChannelTest.php
+++ tests/FcmChannelTest.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 use Mockery;
 use NotificationChannels\Fcm\FcmChannel;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 class FcmChannelTest extends TestCase
 {
@@ -13,8 +14,9 @@ class FcmChannelTest extends TestCase
     public function it_can_be_instantiated()
     {
         $events = Mockery::mock(Dispatcher::class);
+        $logger = Mockery::mock(LoggerInterface::class);
 
-        $channel = new FcmChannel($events);
+        $channel = new FcmChannel($events, $logger);
 
         $this->assertInstanceOf(FcmChannel::class, $channel);
     }
